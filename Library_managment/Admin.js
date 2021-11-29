3fb4f5975132eb5ea1e9bb1d69968506335597b7
ajit-shewale <93571431+ajit-shewale@users.noreply.github.com>
2021-11-29 20:20:48 +0530

    Add files via upload

---
+++ Library_managment/Admin.js
@@ -0,0 +1,64 @@
+function addBook()
+{
+    var x = document.getElementById("addBook");
+    if(x.style.display ==="none")
+    {
+        x.style.display = "block";
+    }
+    else
+    {
+        x.style.display = "none";
+    }
+}
+
+function updateBook()
+{
+    var x = document.getElementById("updateBook");
+    if(x.style.display ==="none")
+    {
+        x.style.display = "block";
+    }
+    else
+    {
+        x.style.display = "none";
+    }
+}
+
+function deleteBook()
+{
+    var x = document.getElementById("deleteBook");
+    if(x.style.display ==="none")
+    {
+        x.style.display = "block";
+    }
+    else
+    {
+        x.style.display = "none";
+    }
+}
+
+function searchBook()
+{
+    var x = document.getElementById("searchBook");
+    if(x.style.display ==="none")
+    {
+        x.style.display = "block";
+    }
+    else
+    {
+        x.style.display = "none";
+    }
+}
+
+function viewOrders()
+{
+    var x = document.getElementById("viewOrders");
+    if(x.style.display ==="none")
+    {
+        x.style.display = "block";
+    }
+    else
+    {
+        x.style.display = "none";
+    }
+}
